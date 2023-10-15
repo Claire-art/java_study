2aefefb7d6f8d2972e3f14da1dbd28c8c74673bc
Claire-art <juachef@gmail.com>
2023-10-15 15:18:25 +0900

    Setting: 자바 개발 환경 셋팅 완료

---
+++ 객체지향프로그래밍/ch/src/ByteTestError.java
@@ -0,0 +1,8 @@
+public class ByteTestError{
+    public static void main(String args[])
+    {
+        byte a = (byte)128;
+        System.out.println("128을 저장한 바이트 값은: " + a);
+    }
+}
+
---
+++ 객체지향프로그래밍/ch/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String args[])
+    {
+        byte a = (byte)128;
+        System.out.println("128을 저장한 바이트 값은: " + a);
+    
+        byte b = (byte)256;
+        System.out.println("256을 저장한 바이트 값은: " +b);
+    }
+}
