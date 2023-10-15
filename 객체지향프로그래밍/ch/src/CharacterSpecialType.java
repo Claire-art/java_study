ac71a9ac6de9c76c44f7760b66e19452e08c8594
Claire-art <juachef@gmail.com>
2023-10-15 16:05:03 +0900

    Add: CharacterSpecialType

---
+++ 객체지향프로그래밍/ch/src/CharacterSpecialType.java
@@ -0,0 +1,18 @@
+public class CharacterSpecialType {
+    public static void main(String args[])
+    {
+        char c1 = '\"';
+        char c2 = '대';
+        char c3 = '한';
+        char c4 = '\"';
+        char c5 = '\t';
+        char c6 = '민';
+        char c7 = '국';
+        char c8 = '\n';
+        char c9 = '만';
+        char c10 ='세';
+
+        System.out.println("출력 결과: \n"
+        +c1+c2+c3+c4+c5+c6+c7+c8+c9+c10);
+    }
+}
